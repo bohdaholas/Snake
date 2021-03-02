f6860b28f9e9e155873a8c48a5e52046f49e7d8f
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2021-03-02 17:52:57 +0200

    a

--- snake/snake.ino
+++ snake/snake.ino
@@ -1,10 +1,24 @@
-class Snake {
-      
-};
+//class Snake {
+//  public:
+//    int body[64][2];
+//    int head_x = random(1, 6);
+//      int head_y = random(2, 4);
+//    void move(int dir) {
+//        if (dir == 2) {
+//          
+//        }
+//        if (dir == -2) {
+//          
+//        }
+//        if (dir == -1) {
+//          
+//        }
+//        if (dir == 1) {
+//          
+//        }
+//    }
+//};
 
-class Apple {
-    
-};
 
 void turn_on_led(int row, int col) {
   turn_off_all_leds();
@@ -20,35 +34,58 @@ void turn_on_led(int row, int col) {
 
 void turn_off_all_leds() {
   for (int i = 0; i <= 13; i++) {
-        pinMode(i, INPUT);
-  }  
-}    
+    pinMode(i, INPUT);
+  }
+}
 
 void turn_on_all_leds() {
   for (int i = 1; i <= 7; i++) {
-      pinMode(i, OUTPUT);
-      digitalWrite(i, 0);
-  }  
+    pinMode(i, OUTPUT);
+    digitalWrite(i, 0);
+  }
   for (int i = 8; i <= 13; i++) {
-      pinMode(i, OUTPUT);
-      digitalWrite(i, 1);
-  }  
+    pinMode(i, OUTPUT);
+    digitalWrite(i, 1);
+  }
 }
 
-void joystick() {
+int joystick() {
   int y = analogRead(A0);
   int x = analogRead(A1);
-  Serial.print(y * 5 / 1023);
+  Serial.print(x * 5 / 1023);
   Serial.print(" ");
-  Serial.println(x * 5 / 1023);  
+  Serial.println(y * 5 / 1023);
+  if (x == 2 && y > 2) {
+    // up
+    return 2; 
+  }
+  if (x == 2 && y < 2) {
+    // down
+    return -2;  
+  }
+  if (x > 3 && y > 3) {
+    // left
+    return -1;  
+  }
+  if (x > 3 && y > 3) {
+    // right
+    return 1;   
+  }
+  return 0;
 }
 
 void setup() {
-    pinMode(A0, INPUT);
-    pinMode(A1, INPUT);
-    Serial.begin(9600);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  Serial.begin(9600);
 }
 
+
+int snake_head_x = random(1, 6);
+int snake_head_y = random(1, 6);
+int body[36][2];
 void loop() {
-  
+  joystick();
+  delay(200);
+  turn_on_led(snake_head_x, snake_head_y);
 }
