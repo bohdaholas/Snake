0d3d938bf6101b9edc0864dfe86e48f713fe59c5
archy-co <yaroslav.moskalyk@ucu.edu.ua>
2021-03-02 20:10:55 +0200

    Single led movement implementation

--- snake/snake.ino
+++ snake/snake.ino
@@ -1,6 +1,27 @@
 int snake_body_x[64] = {random(1, 6)};
 int snake_body_y[64] = {random(1, 6)};
 
+int apple_on_field = false;
+
+void generate_apple() {
+  int row, column;
+  row = random(1, 6);
+  column = random(1, 6);
+  turn_on_additional_led(row, column);
+  apple_on_field = true;
+}
+
+void turn_on_additional_led(int row, int col) {
+  if (1 <= row <= 6) {
+    pinMode(row + 1, OUTPUT);
+    digitalWrite(row + 1, 0);
+  }
+  if (1 <= col <= 6) {
+    pinMode(col + 7, OUTPUT);
+    digitalWrite(col + 7, 1);
+  }
+}
+
 void turn_on_led(int row, int col) {
   turn_off_all_leds();
   if (1 <= row <= 6) {
@@ -33,25 +54,34 @@ void turn_on_all_leds() {
 int joystick() {
   int y = analogRead(A0) * 5 / 1023;
   int x = analogRead(A1) * 5 / 1023;
-  Serial.print(x * 5 / 1023);
-  Serial.print(" ");
-  Serial.println(y * 5 / 1023);
+  Serial.print(x);
+  Serial.print("     ");
+  Serial.println(y);
   if (x == 2 && y > 2) {
     // up
-    return 2; 
+    return -2; 
   }
   if (x == 2 && y < 2) {
     // down
-    return -2;  
+    return 2;  
   }
-  if (x > 3 && y > 3) {
+  if (x == 5) {
+    return -1;
+  }
+  if (x == 0) {
+    // right
+    return 1;   
+  }
+  
+  /*if (x > 3 && y > 3) {
     // left
     return -1;  
   }
   if (x > 3 && y > 3) {
     // right
     return 1;   
-  }
+  }*/
+  
   return 0;
 }
 
@@ -81,13 +111,17 @@ void snake_move(int dir) {
   if (snake_body_x[0] == 7) {
     snake_body_x[0] = 1;
   }
-  if (snake_body_x[0] == -1) {
+  if (snake_body_x[0] == 0) {
     snake_body_x[0] = 6;
   }
+  
+  // joystick down
   if (snake_body_y[0] == 7) {
     snake_body_y[0] = 1;
   }
-  if (snake_body_y[0] == -1) {
+
+  // joystick up
+  if (snake_body_y[0] == 0) {
     snake_body_y[0] = 6;
   }
   turn_on_led(snake_body_x[0], snake_body_y[0]);
@@ -96,5 +130,8 @@ void snake_move(int dir) {
 void loop() {
   int dir = joystick();
   snake_move(dir);
+  if (!apple_on_field) {
+    generate_apple();
+  }
   delay(200);
 }
